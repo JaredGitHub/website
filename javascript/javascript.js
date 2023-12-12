9eda5c873367b8dd31f1c034e465fc649ab0f1a2
JaredGitHub <45084265+JaredGitHub@users.noreply.github.com>
2023-12-12 13:40:03 -0700

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,46 +1,72 @@
-body.addEventListener("click", explodeOnClick);
-body.addEventListener("keydown", keyPress);
+//Whenever someone presses the e key Elon Musk will show up for 3 seconds and say AI unleashes a demon.
 
-function keyPress(event)
+$("#elonswitch").change(function()
 {
-  if (event.key == "e")
-  {
+  var elonswitch = $(this).is(':checked');
+
+  if(elonswitch === true)
+{
+  $("body").on("keypress", function (event) {
+    if (event.key == "e") {
       let img = document.createElement("img");
-    
-      img.src = "https://www.hollywoodreporter.com/wp-content/uploads/2022/05/Musk-2.jpg?w=2000&h=1126&crop=1";
+  
+      img.src = "../images/elon musk.png";
       document.body.appendChild(img);
-
-      let audio = new Audio("website/audio/demon.mp3");
+  
+      let audio = new Audio("../audio/demon.mp3");
       audio.play();
-
+  
       img.style.position = "fixed";
       img.style.height = "800px";
       img.style.left = "10px";
       img.style.top = "10px";
-
+      $(img).show();
+  
       setTimeout(() => {
         img.remove();
+        $(img).hide();
       }, 3000);
+    }
+  });
+}
+else
+{
+  $("body").off("keypress");
+}
+});
+
+//Checking if the user clicks on the page, and wherever they click it will put an explosion gif and a sound!
+$("#explosionswitch").change(function ()
+{
+  var explosionswitch = $(this).is(':checked');
+
+  if (explosionswitch === true)
+  {
+    $("body").on("click", function (event) {
+      let x = event.pageX;
+      let y = event.pageY;
+
+      let explosion = document.getElementsByClassName("explosion");
+
+      let img = new Image();
+      let audio = new Audio("../audio/explosion.mp3");
+      audio.play();
+
+      img.src = "../images/explode-boom.gif";
+      document.getElementsByClassName("explosion")[0].appendChild(img);
+
+      img.style.position = "absolute";
+      img.style.left = x - 30 + "px";
+      img.style.top = y - 50 + "px";
+      $(".explosion").show();
+
+      setTimeout(() => {
+        $(".explosion").hide();
+        img.remove();
+      }, 700);
+    });
   }
-}
-function explodeOnClick(event) {
-  let x = event.pageX;
-  let y = event.pageY;
-
-  let explosion = document.getElementsByClassName("explosion");
-
-  let img = new Image();
-  let audio = new Audio("website/audio/explosion.mp3");
-  audio.play();
-
-  img.src = "https://media.tenor.com/j-ixpElDGOYAAAAi/explode-boom.gif";
-  document.getElementsByClassName("explosion")[0].appendChild(img);
-
-  img.style.position = "absolute";
-  img.style.left = x - 30 + "px";
-  img.style.top = y - 50 + "px";
-
-  setTimeout(() => {
-    img.remove();
-  }, 700);
-}
+  else{
+    $("body").off("click");
+  }
+});
